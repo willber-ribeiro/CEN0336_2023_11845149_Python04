6b0fc5264877cf05de2ac720c64037ece80980e8
will <93087026+willber-ribeiro@users.noreply.github.com>
2023-10-12 00:10:07 -0300

    Update Python04_4.py

--- Python04_4.py
+++ Python04_4.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-# Write a script that uses a while loop to print out the numbers 1 to 100.
+# 4. Write a script that uses a while loop to print out the numbers 1 to 100.
 number = 1
 while number < 101:
     print(number)
